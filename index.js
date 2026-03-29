df11bb41439284990b2510357c3d9f788819ae81
kirillrogolev-max <kirill.rogolev@gmail.com>
2026-03-29 21:40:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.TOKEN;
+const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
